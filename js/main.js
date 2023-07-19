d5baa2d2412c41b1b89fbda614284322f3cfccd2
umbertofaenza <umberto.faenza@gmail.com>
2023-07-19 17:31:56 +0200

    feat_js: add km and age validity checks

--- js/main.js
+++ js/main.js
@@ -8,17 +8,28 @@ const passengerAge = parseInt(prompt("Quanti anni ha il passeggero?"));
 let basePrice = userKM * 0.21;
 let discountDescription = "0%";
 
-// Passeggero minorenne
-if (passengerAge < 18) {
-  basePrice = basePrice - (basePrice * 20) / 100;
-  discountDescription = "20% - Sconto per minorenni";
-  // Passeggero over 65
-} else if (passengerAge >= 65) {
-  basePrice = basePrice - (basePrice * 40) / 100;
-  discountDescription = "40% - Sconto per Over 65";
+if (
+  userKM &&
+  userKM > 0 &&
+  passengerAge &&
+  passengerAge > 0 &&
+  passengerAge < 150
+) {
+  // Passeggero minorenne
+  if (passengerAge < 18) {
+    basePrice = basePrice - (basePrice * 20) / 100;
+    discountDescription = "20% - Sconto per minorenni";
+    // Passeggero over 65
+  } else if (passengerAge >= 65) {
+    basePrice = basePrice - (basePrice * 40) / 100;
+    discountDescription = "40% - Sconto per Over 65";
+  }
+  // Prezzo finale e sconto
+  document.getElementById("ticket-price__generated").innerHTML =
+    "€" + basePrice.toFixed(2);
+  document.getElementById("discount").innerHTML = discountDescription;
+} else if (!userKM || userKM <= 0) {
+  alert("Valore km invalido!");
+} else if (!passengerAge || passengerAge <= 0 || passengerAge >= 150) {
+  alert("Valore età invalido!");
 }
-
-// Prezzo finale e sconto
-document.getElementById("ticket-price__generated").innerHTML =
-  "€" + basePrice.toFixed(2);
-document.getElementById("discount").innerHTML = discountDescription;
