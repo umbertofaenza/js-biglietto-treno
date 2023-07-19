bf64fe10f3c0c5f809a058e566212364f9f1ba6d
umbertofaenza <umberto.faenza@gmail.com>
2023-07-19 16:08:25 +0200

    feat_js: add if cycle

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ const basePrice = userKM * 0.21;
 
 // Dichiaro una variabile per il prezzo scontato
 let discountedPrice;
+
+if (passengerAge < 18) {
+  discountedPrice = basePrice - (basePrice * 20) / 100;
+  document.getElementById("ticket-price__generated").innerHTML =
+    "€" + discountedPrice;
+  document.getElementById("discount").innerHTML = "20% - Sconto per minorenni";
+} else if (passengerAge >= 65) {
+  discountedPrice = basePrice - (basePrice * 40) / 100;
+  document.getElementById("ticket-price__generated").innerHTML =
+    "€" + discountedPrice;
+  document.getElementById("discount").innerHTML = "40% - Sconto per Over 65";
+} else {
+  document.getElementById("ticket-price__generated").innerHTML =
+    "€" + basePrice;
+  document.getElementById("discount").innerHTML = "0%";
+}
